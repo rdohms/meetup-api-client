62c63273f79b4af9b557b2855393c66c063054a3
Rafael Dohms <rdohms@gmail.com>
2014-04-06 13:22:01 +0200

    Adding test for bug #8
    Unable to reproduce bug.

---
+++ tests/Functional/Bug8Test.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Functional;
+
+use Test\Framework\FunctionalTestCase;
+
+class Bug8Test extends FunctionalTestCase
+{
+    public function testGetGroups()
+    {
+        $result = $this->keyClient->getGroups(array('country' => 'NL', 'city' => 'Amsterdam', 'topic' => 'php'));
+
+        $this->assertInstanceOf('DMS\Service\Meetup\Response\MultiResultResponse', $result);
+    }
+
+    public function testGetGroupsSingle()
+    {
+        $result = $this->keyClient->getGroups(array('group_urlname' => 'amsterdamphp'));
+
+        $this->assertInstanceOf('DMS\Service\Meetup\Response\MultiResultResponse', $result);
+    }
+}
---
+++ tests/Test/Framework/FunctionalTestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Test\Framework;
+
+use DMS\Service\Meetup\MeetupKeyAuthClient;
+use PHPUnit_Framework_TestCase;
+
+class FunctionalTestCase extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var MeetupKeyAuthClient
+     */
+    protected $keyClient;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $configFile = __DIR__ . '/../../api_key.ini';
+
+        if (! file_exists($configFile)) {
+            $this->markTestSkipped('Functional Tests require an Oauth Key');
+        }
+
+        $config = parse_ini_file($configFile);
+
+        $this->keyClient = MeetupKeyAuthClient::factory(array('key' => $config['key']));
+    }
+
+}
