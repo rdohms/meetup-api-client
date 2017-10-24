5bc4c7874e989e156366a5861bd861a2a34a3437
Rafael Dohms <rdohms@gmail.com>
2017-10-24 22:14:59 +0200

    Create custom exceptions
    In order to wrap Client and Server errors in easier to handle exceptions

---
+++ src/DMS/Service/Meetup/Exception/ServerException.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DMS\Service\Meetup\Exception;
+
+use GuzzleHttp\Command\Exception\CommandClientException;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+final class ServerException extends \Exception
+{
+    /** @var  CommandClientException */
+    protected $previous;
+
+    public static function fromGuzzleException(CommandClientException $exception): self
+    {
+        $instance = new static($exception->getMessage(), $exception->getResponse()->getStatusCode());
+        $instance->previous = $exception;
+        return $instance;
+    }
+
+    public function getRequest(): RequestInterface
+    {
+        return $this->previous->getRequest();
+    }
+
+    public function getResponse(): ResponseInterface
+    {
+        return $this->previous->getResponse();
+    }
+}
